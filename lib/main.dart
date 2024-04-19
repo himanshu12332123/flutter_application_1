bcd8a13969e2e71021759e69f8a168238004b889
himanshu12332123 <105910418+himanshu12332123@users.noreply.github.com>
2024-04-20 04:13:55 +0530

    updated

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+             gradient: LinearGradient(
+              colors: [
+                Colors.black,
+                  Colors.redAccent,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              )
+          ),
+          child: const Center(
+            child:Image.asset()
+            ) ),
+          ),
+        ),
+        
+      ),
+    );
+  }
+}
